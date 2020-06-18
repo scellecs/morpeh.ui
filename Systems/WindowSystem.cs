1a68025f65a517ba752ee5fd0d9b4c2460a2b81d
golovchanskyva <golovchanskyva@plavalaguna.ru>
2020-06-18 23:28:29 +0300

    add simple  ui manager

--- Systems/WindowSystem.cs
+++ Systems/WindowSystem.cs
@@ -1,4 +1,5 @@
 ﻿using Morpeh;
+using Morpeh.UI;
 using Morpeh.UI.Components;
 using UnityEngine;
 using Unity.IL2CPP.CompilerServices;
@@ -10,31 +11,65 @@ using Unity.IL2CPP.CompilerServices;
 [Il2CppSetOption(Option.DivideByZeroChecks, false)]
 [CreateAssetMenu(menuName = "ECS/Systems/" + nameof(WindowSystem))]
 public sealed class WindowSystem : UpdateSystem {
-    private Filter filter;
+    private Filter windows;
+    private Filter fullScreenWindows;
+    private Filter openedFullScreenWindow;
     
     public override void OnAwake() {
-        this.filter = this.World.Filter.With<WindowComponent>();
+        this.windows = this.World.Filter.With<WindowComponent>().Without<FullScreenWindowComponent>();
+        this.fullScreenWindows = this.World.Filter.With<WindowComponent>().With<FullScreenWindowComponent>();
+        this.openedFullScreenWindow = this.World.Filter.With<WindowComponent>().Without<FullScreenWindowComponent>()
+            .With<OpenedFullScreenWindowMarker>();
     }
 
     public override void OnUpdate(float deltaTime) {
-        foreach (var entity in this.filter) {
+        foreach (var entity in this.windows) {
             ref var w = ref entity.GetComponent<WindowComponent>();
             if (w.openEvent) {
-                w.canvasGroup.alpha        = 1f;
-                w.canvasGroup.interactable = true;
-                w.canvasGroup.blocksRaycasts = true;
-                if (w.raycaster != null) {
-                    w.raycaster.enabled = true;
+                this.SetActive(ref w, true);
+            }
+            else if (w.closeEvent)
+            {
+                this.SetActive(ref w, false);
+            }
+        }
+
+        foreach (var entity in this.fullScreenWindows)
+        {
+            ref var w = ref entity.GetComponent<WindowComponent>();
+            if (w.openEvent) {
+                if (entity.Has<ForceCloseOthersFullScreenComponent>() && this.openedFullScreenWindow.Length != 0)
+                {
+                    var opened = this.openedFullScreenWindow.First();
+                    opened.RemoveComponent<OpenedFullScreenWindowMarker>();
+                    this.SetActive(ref opened.GetComponent<WindowComponent>(), false);
+                }
+                else if (!entity.Has<ForceCloseOthersFullScreenComponent>() && this.openedFullScreenWindow.Length != 0)
+                {
+                    //ToDo: add to queue
+                }
+                else if (!entity.Has<ForceCloseOthersFullScreenComponent>() && this.openedFullScreenWindow.Length == 0)
+                {
+                    entity.SetComponent(new OpenedFullScreenWindowMarker());
+                    this.SetActive(ref w, true);
                 }
             }
-            else if (w.closeEvent) {
-                w.canvasGroup.alpha = 0f;
-                w.canvasGroup.interactable = false;
-                w.canvasGroup.blocksRaycasts = false;
-                if (w.raycaster != null) {
-                    w.raycaster.enabled = false;
-                }
+            else if (w.closeEvent)
+            {
+                entity.RemoveComponent<OpenedFullScreenWindowMarker>();
+                this.SetActive(ref w, false);
+                
+                //ToDo: check queue and show first
             }
+        }
+    }
+
+    private void SetActive(ref WindowComponent w, bool value) {
+        w.canvasGroup.alpha = value ? 1f : 0f;
+        w.canvasGroup.interactable = value;
+        w.canvasGroup.blocksRaycasts = value;
+        if (w.raycaster != null) {
+            w.raycaster.enabled = value;
         }
     }
 }
